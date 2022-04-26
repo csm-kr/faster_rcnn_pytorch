beb57056c94c0dba299fdd725d14d43d1e1a6ec4
csm-kr <csm8167@naver.com>
2022-04-26 18:36:47 +0900

    set rpn traning version1

--- image_test.py
+++ image_test.py
@@ -18,7 +18,7 @@ this code is related to the shape of various image processing library
 root = './figures'
 image_path = os.path.join(root, '000001.jpg')
 
-# -------------------------------------------#
+# ------------------------------,-------------#
 # 1. PIL
 # -------------------------------------------#
 
@@ -37,28 +37,28 @@ image_path = os.path.join(root, '000001.jpg')
 # # 1-4) format / dtype / range
 # print(img_pil.mode)
 # print(img_pil.bits)
-
-# -------------------------------------------#
-# 2. cv2
-# -------------------------------------------#
-
-import cv2
-import numpy as np
-
-# 2-1) make PIL image
-img_cv2 = cv2.imread(image_path)
-# img_cv2[100][50] = np.array([255, 255, 255], dtype=np.uint8)
-
-# 2-2) visualize PIL image
-cv2.imshow('img_cv2', img_cv2)
-cv2.waitKey(0)
-
-# 2-3) size of cv2 image
-print(img_cv2.shape)
-
-# 2-4) format / dtype / range of cv2 image
-print(img_cv2.dtype)
-# -
+#
+# # -------------------------------------------#
+# # 2. cv2
+# # -------------------------------------------#
+#
+# import cv2
+# import numpy as np
+#
+# # 2-1) make PIL image
+# img_cv2 = cv2.imread(image_path)
+# # img_cv2[100][50] = np.array([255, 255, 255], dtype=np.uint8)
+#
+# # 2-2) visualize PIL image
+# cv2.imshow('img_cv2', img_cv2)
+# cv2.waitKey(0)
+#
+# # 2-3) size of cv2 image
+# print(img_cv2.shape)
+#
+# # 2-4) format / dtype / range of cv2 image
+# print(img_cv2.dtype)
+# # -
 # print(img_cv2.min, img_cv2.max)
 
 # -------------------------------------------#
@@ -68,6 +68,21 @@ print(img_cv2.dtype)
 # 3-1) make tensor
 img_pil = Image.open(image_path)
 img_tensor = F.to_tensor(img_pil)
+
+# 3-2) visualize tensor image
+
+# 3-3) size of tensor image
+print(img_tensor.size())
+print(img_tensor.shape)
+
+# # 2-4) format / dtype / range of cv2 image
+# print(img_cv2.dtype)
+print(img_tensor.dtype)
+# print(img_cv2.min, img_cv2.max)
+
+plt.figure('input')
+plt.imshow(img_tensor.permute(1, 2, 0))
+plt.show()
 
 # question
 # img_tensor[:, 50, 100] = torch.Tensor([0, 0, 0])
--- model/rpn.py
+++ model/rpn.py
@@ -9,8 +9,23 @@ class RPN(nn.Module):
         super().__init__()
         self.conv_layers = nn.Sequential(*list(conv_layers.features.children())[:-1])  # after conv 5_3
         self.intermediate_layer = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
-        self.reg_layer = nn.Conv2d(in_channels=512, out_channels=num_anchors * 2, kernel_size=1)
-        self.cls_layer = nn.Conv2d(in_channels=512, out_channels=num_anchors * 4, kernel_size=1)
+        self.cls_layer = nn.Conv2d(in_channels=512, out_channels=num_anchors * 2, kernel_size=1)
+        self.reg_layer = nn.Conv2d(in_channels=512, out_channels=num_anchors * 4, kernel_size=1)
+        self.initialize()
+
+    def initialize(self):
+        for c in self.intermediate_layer.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.normal_(c.weight, std=0.01)
+                nn.init.constant_(c.bias, 0)
+        for c in self.cls_layer.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.normal_(c.weight, std=0.01)
+                nn.init.constant_(c.bias, 0)
+        for c in self.reg_layer.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.normal_(c.weight, std=0.01)
+                nn.init.constant_(c.bias, 0)
 
     def forward(self, x):
         x = self.conv_layers(x)
@@ -21,7 +36,7 @@ class RPN(nn.Module):
 
 
 if __name__ == '__main__':
-    img = torch.randn([2, 3, 600, 1000])  # 37, 62
+    img = torch.randn([2, 3, 1000, 600])  # 62, 37
     rpn = RPN(conv_layers=vgg16_bn(pretrained=True))
     cls, reg = rpn(img)
 
