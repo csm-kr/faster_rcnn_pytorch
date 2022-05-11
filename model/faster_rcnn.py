8213752f83b5d7d69fcd4c4449f67397e80c8cd0
csm-kr <csm8167@naver.com>
2022-05-11 16:10:19 +0900

    refactoring frcnn model

--- model/faster_rcnn.py
+++ model/faster_rcnn.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
-from torchvision.models import vgg16_bn
+from torchvision.models import vgg16
 from torchvision.ops import RoIPool
 import numpy as np
 from utils import propose_region
 from model.rpn import RPN
+from anchor import FRCNNAnchorMaker
+import time
 
 
 class FRCNNHead(nn.Module):
@@ -12,7 +14,7 @@ class FRCNNHead(nn.Module):
         super().__init__()
         self.num_classes = num_classes
         self.cls_head = nn.Linear(4096, num_classes)
-        self.loc_head = nn.Linear(4096, num_classes * 4)
+        self.reg_head = nn.Linear(4096, 4)
         self.roi_pool = RoIPool(output_size=(roi_output_size, roi_output_size), spatial_scale=1.)
         self.fc = nn.Sequential(nn.Linear(512 * 7 * 7, 4096),
                                 nn.ReLU(inplace=True),
@@ -26,7 +28,7 @@ class FRCNNHead(nn.Module):
             if isinstance(c, nn.Linear):
                 nn.init.normal_(c.weight, std=0.01)
                 nn.init.constant_(c.bias, 0)
-        for c in self.loc_head.children():
+        for c in self.reg_head.children():
             if isinstance(c, nn.Linear):
                 nn.init.normal_(c.weight, std=0.01)
                 nn.init.constant_(c.bias, 0)
@@ -35,60 +37,73 @@ class FRCNNHead(nn.Module):
                 nn.init.normal_(c.weight, std=0.01)
                 nn.init.constant_(c.bias, 0)
 
-    def forward(self, x_features, boxes):
-        device = boxes.get_device()
-        filtered_boxes_numpy = np.array(boxes.detach().cpu()).astype(np.float32)
+    def forward(self, features, rois):
+
+        device = rois.get_device()
+        filtered_rois_numpy = np.array(rois.detach().cpu()).astype(np.float32)
 
         # 3. scale original box w, h to feature w, h
-        filtered_boxes_numpy[:, ::2] = filtered_boxes_numpy[:, ::2] * x_features.size(3)
-        filtered_boxes_numpy[:, 1::2] = filtered_boxes_numpy[:, 1::2] * x_features.size(2)
+        filtered_rois_numpy[:, ::2] = filtered_rois_numpy[:, ::2] * features.size(3)
+        filtered_rois_numpy[:, 1::2] = filtered_rois_numpy[:, 1::2] * features.size(2)
 
         # 4. convert numpy boxes to list of tensors
-        filtered_boxes_tensor = [torch.FloatTensor(filtered_boxes_numpy).to(device)]
+        filtered_boxes_tensor = [torch.FloatTensor(filtered_rois_numpy).to(device)]
 
         # --------------- RoI Pooling --------------- #
-        x = self.roi_pool(x_features, filtered_boxes_tensor)           # [2000, 512, 7, 7]
+        x = self.roi_pool(features, filtered_boxes_tensor)           # [2000, 512, 7, 7]
         x = x.view(x.size(0), -1)                                      # 2000, 512 * 7 * 7
 
         # --------------- forward head --------------- #
         x = self.fc(x)                                                 # 2000, 4096
         cls = self.cls_head(x)                                         # 2000, 21
-        loc = self.loc_head(x)                                         # 2000, 21 * 4
-        cls = cls.view(1, -1, self.num_classes)                        # [1, 2000, 21]
-        loc = loc.view(1, -1, self.num_classes * 4)                    # [1, 2000, 21 * 4]
-        return cls, loc
+        reg = self.reg_head(x)                                         # 2000, 21 * 4
+        frcnn_cls = cls.view(1, -1, self.num_classes)                        # [1, 2000, 21]
+        frcnn_reg = reg.view(1, -1, 4)                    # [1, 2000, 21 * 4]
+        return frcnn_cls, frcnn_reg
 
 
-class FasterRCNN(nn.Module):
-    def __init__(self, rpn, head, coder):
+class FRCNN(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.rpn = rpn
-        self.head = head
-        self.coder = coder
+
+        # ** for forward
+        self.extractor = nn.Sequential(*list(vgg16(pretrained=True).features.children())[:-1])
+        self.rpn = RPN()
+        self.head = FRCNNHead(num_classes=21)
+
+        # ** for anchor
+        self.anchor_maker = FRCNNAnchorMaker()
+        self.anchor_base = self.anchor_maker.generate_anchor_base()
 
     def forward(self, x):
-        cls_rpn, reg_rpn, x_features = self.rpn(x, True)
-        pred = (cls_rpn, reg_rpn)
-        pred_boxes = propose_region(pred=pred,
-                                    coder=self.coder)
-        cls, reg = self.head(x_features, pred_boxes)
-        return cls, reg
+        # x : image [B, 3, H, W]
+        features = self.extractor(x)
+
+        # each image has different anchor
+        anchor = self.anchor_maker._enumerate_shifted_anchor(self.anchor_base, origin_image_size=x.size()[2:])
+        anchor = torch.from_numpy(anchor).to(x.get_device())  # assign device
+        cls_rpn, reg_rpn, rois = self.rpn(features, anchor, mode='train')
+        cls_frcnn, reg_frcnn = self.head(features, rois)
+
+        return cls_rpn, reg_rpn, cls_frcnn, reg_frcnn, anchor
 
 
 if __name__ == '__main__':
-    img = torch.randn([1, 3, 600, 1000])  # 37, 62
-    img = torch.randn([1, 3, 800, 800]).cuda()  # 37, 62
-    rpn = RPN(vgg16_bn(pretrained=True))
-    head = FRCNNHead(num_classes=20, roi_output_size=7)
-    from coder import FasterRCNN_Coder
-    coder = FasterRCNN_Coder()
-    coder.set_anchors((800, 800))
-    coder.center_anchor = coder.center_anchor.cuda()
-    Fasterrcnn = FasterRCNN(rpn, head, coder=coder).cuda()
-    cls, reg = Fasterrcnn(img)
 
-    print(cls.size())
-    print(reg.size())
+    tic = time.time()
+    frcnn = FRCNN().cuda()
+    print("model cuda time :", time.time() - tic)
 
+    tic = time.time()
+    img1 = torch.randn([1, 3, 600, 1000]).cuda()  # 37, 62
+    print("image cuda time :", time.time() - tic)
 
+    tic = time.time()
+    rpn_cls, rpn_reg, frcnn_cls, frcnn_reg, anchor = frcnn(img1)
+
+    print(rpn_cls.size())     # torch.Size([1, 18, 37, 62])
+    print(rpn_reg.size())     # torch.Size([1, 36, 37, 62])
+    print(frcnn_cls.size())   # torch.Size([1, 1988, 21])
+    print(frcnn_reg.size())   # torch.Size([1, 1988, 4])
+    print(anchor.size())      # torch.Size([20646, 4])
 
--- model/rpn.py
+++ model/rpn.py
@@ -2,15 +2,70 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 import torch.nn as nn
 from torchvision.models import vgg16_bn
+from utils import cxcy_to_xy, decode
+from torchvision.ops.boxes import nms
+
+
+class RegionProposal(nn.Module):
+    def __init__(self, min_size=16/1000):
+        super().__init__()
+        self.min_size = min_size
+
+    def forward(self, pred, anchor, mode='test'):
+
+        pre_nms_top_k = 12000
+        post_num_top_k = 2000
+        if mode == 'test':
+            pre_nms_top_k = 6000
+            post_num_top_k = 300
+
+        # 1. setting for make roi
+        pred_cls, pred_loc = pred
+        batch_size = pred_cls.size(0)
+        pred_cls = pred_cls.permute(0, 2, 3, 1).contiguous()  # [B, C, H, W] to [B, H, W, C]
+        pred_loc = pred_loc.permute(0, 2, 3, 1).contiguous()  # [B, C, H, W] to [B, H, W, C]
+        pred_cls = pred_cls.reshape(batch_size, -1, 2)
+        pred_loc = pred_loc.reshape(batch_size, -1, 4)
+
+        # 2. pred to roi
+        roi = pred_bboxes = cxcy_to_xy(decode(pred_loc.squeeze(), anchor)).clamp(0, 1)     # for batch 1, [67995, 4]
+        pred_scores = pred_cls.squeeze()                                         # for batch 1, [67995, num_classes]
+
+        # TODO pred scores to softmax or sigmoid
+        pred_scores = torch.sigmoid(pred_scores)
+
+        # 2. minimum size keep
+        # ws = roi[:, 2] - roi[:, 0]
+        # hs = roi[:, 3] - roi[:, 1]
+        # TODO np to torch
+        # keep = np.where((hs >= self.min_size) & (ws >= self.min_size))[0]
+        # roi = roi[keep, :]
+        # score = score[keep]
+
+        # 3. nms keep
+        sorted_scores, sorted_idx_scores = pred_scores[..., 1].squeeze().sort(descending=True)
+        if len(sorted_idx_scores) < pre_nms_top_k:
+            pre_nms_top_k = len(sorted_idx_scores)
+        roi = roi[sorted_idx_scores[:pre_nms_top_k]]  # [12000, 4]
+        roi = roi[:pre_nms_top_k]  # [12000]
+        sorted_scores = sorted_scores[:pre_nms_top_k]  # [12000]
+
+        keep_idx = nms(boxes=roi, scores=sorted_scores, iou_threshold=0.7)
+        keep = torch.zeros(pre_nms_top_k, dtype=torch.bool)
+        keep[keep_idx] = 1  # int64 to bool
+        roi = roi[keep][:post_num_top_k]
+
+        return roi
 
 
 class RPN(nn.Module):
-    def __init__(self, conv_layers, num_anchors=9):
+    def __init__(self):
         super().__init__()
-        self.conv_layers = nn.Sequential(*list(conv_layers.features.children())[:-1])  # after conv 5_3
+        num_anchors = 9
         self.intermediate_layer = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
         self.cls_layer = nn.Conv2d(in_channels=512, out_channels=num_anchors * 2, kernel_size=1)
         self.reg_layer = nn.Conv2d(in_channels=512, out_channels=num_anchors * 4, kernel_size=1)
+        self.region_proposal = RegionProposal()
         self.initialize()
 
     def initialize(self):
@@ -27,19 +82,18 @@ class RPN(nn.Module):
                 nn.init.normal_(c.weight, std=0.01)
                 nn.init.constant_(c.bias, 0)
 
-    def forward(self, x, frcnn_train=False):
-        x = self.conv_layers(x)
-        x = self.intermediate_layer(x)
+    def forward(self, features, anchor, mode):
+
+        x = self.intermediate_layer(features)
         cls = self.cls_layer(x)
         reg = self.reg_layer(x)
-        if frcnn_train:
-             return cls, reg, x
-        return cls, reg
+        roi = self.region_proposal((cls, reg), anchor, mode)
+        return cls, reg, roi
 
 
 if __name__ == '__main__':
     img = torch.randn([2, 3, 1000, 600])  # 62, 37
-    rpn = RPN(conv_layers=vgg16_bn(pretrained=True))
+    rpn = RPN()
     cls, reg = rpn(img)
 
     print(cls.size())
