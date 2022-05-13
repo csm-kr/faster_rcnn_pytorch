ce2778764fa5551cc17aead1ec79378773e17974
csm-kr <csm8167@naver.com>
2022-05-13 17:52:02 +0900

    make build_rpn_target"

--- loss/rpn_loss.py
+++ loss/rpn_loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from utils import find_jaccard_overlap, encode, xy_to_cxcy
 
 
 class SmoothL1Loss(nn.Module):
@@ -15,15 +16,73 @@ class SmoothL1Loss(nn.Module):
 
 
 class RPNLoss(torch.nn.Module):
-    def __init__(self, coder):
+    def __init__(self):
         super().__init__()
-        self.coder = coder
-        self.num_classes = self.coder.num_classes
+        # self.coder = coder
+        # self.num_classes = self.coder.num_classes
         self.bce = nn.BCELoss(reduction='none')
         self.smooth_l1_loss = nn.SmoothL1Loss(reduction='none')
         # self.smooth_l1_loss = SmoothL1Loss()
 
-    def forward(self, pred, boxes, labels, size):
+    def build_rpn_target(self, bbox, anchor):
+        '''
+        bbox : list of tensor [tensor]
+        '''
+
+        # 1. anchor cross boundary 만 걸러내기
+        bbox = bbox[0]  # remove the list for batch : shape [num_obj, 4]
+        anchor_keep = ((anchor[:, 0] >= 0) & (anchor[:, 1] >= 0) & (anchor[:, 2] < 1) & (anchor[:, 3] < 1))
+        anchor = anchor[anchor_keep]
+        num_anchors = anchor.size(0)
+
+        # 2. iou 따라 label 만들기
+        # if label is 1 (positive), 0 (negative), -1 (ignore)
+        label = -1 * torch.ones(num_anchors, dtype=torch.float32, device=bbox.get_device())
+
+        iou = find_jaccard_overlap(anchor, bbox)  # [num anchors, num objects]
+        IoU_max, IoU_argmax = iou.max(dim=1)
+
+        # 2-1. set negative label
+        label[IoU_max < 0.3] = 0
+        # 2-2. set positive label that have highest iou.
+        _, IoU_argmax_per_object = iou.max(dim=0)
+        label[IoU_argmax_per_object] = 1
+        # 2-3. set positive label
+        label[IoU_max > 0.7] = 1
+        # 2-4 sample target
+        n_pos = (label == 1).sum()
+        n_neg = (label == 0).sum()
+
+        if n_pos > 128:
+            pos_indices = torch.arange(label.size(0))[label == 1]
+            perm = torch.randperm(pos_indices.size(0))
+            label[pos_indices[perm[128 - n_pos:]]] = -1  # convert pos label to ignore label
+
+        if n_neg > 128:
+
+            if n_pos >= 128:
+                neg_indices = torch.arange(label.size(0))[label == 0]
+                perm = torch.randperm(neg_indices.size(0))
+                label[neg_indices[perm[128 - n_neg:]]] = -1  # convert neg label to ignore label
+
+            # fewer that 128 positive samples ,pad negative ones
+            if n_pos < 128:
+                neg_indices = torch.arange(label.size(0))[label == 0]
+                perm = torch.randperm(neg_indices.size(0))
+                label[neg_indices[perm[(256 - n_pos) - n_neg:]]] = -1  # convert neg label to ignore label
+
+        assert (label == 1).sum() + (label == 0).sum() == 256
+
+        print((label == 1).sum())
+        print((label == 0).sum())
+        # 3. bbox encoding
+        tg_cxywh = encode(xy_to_cxcy(bbox[IoU_argmax]), xy_to_cxcy(anchor))
+
+        rpn_tg_cls = label
+        rpn_tg_loc = tg_cxywh
+        return rpn_tg_cls, rpn_tg_loc
+
+    def forward(self, pred, boxes, labels, anchors):
         pred_cls, pred_reg = pred
 
         batch_size = pred_cls.size(0)
@@ -31,6 +90,9 @@ class RPNLoss(torch.nn.Module):
         pred_reg = pred_reg.permute(0, 2, 3, 1)  # [B, C, H, W] to [B, H, W, C]
         pred_cls = pred_cls.reshape(batch_size, -1, 2)
         pred_reg = pred_reg.reshape(batch_size, -1, 4)
+
+        # build rpn target
+        rpn_tg_cls, rpn_tg_loc = self.build_rpn_target(boxes, anchors)
 
         self.coder.set_anchors(size=size)
         gt_t_cls, gt_t_reg, anchor_identifier = self.coder.build_target(boxes, labels)
