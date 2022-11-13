2017283810138dbad7549a385b332e5c1aec9983
csm-kr <csm8167@naver.com>
2022-11-13 22:12:16 +0900

    organize the configs and config.py

--- config.py
+++ config.py
@@ -2,21 +2,18 @@ import configargparse
 
 
 def get_args_parser():
+    # config
     parser = configargparse.ArgumentParser(add_help=False)
-    parser.add_argument('--config', is_config_file=True, help='config file path')
-    parser.add_argument('--name', type=str)
+    parser.add_argument('--config', is_config_file=True, help='config file path')       # TODO
+    parser.add_argument('--name', type=str)                                             # TODO
 
-    # visualization
-    parser.add_argument('--visdom_port', type=int, default=8899)
+    # visdom
+    parser.add_argument('--visdom_port', type=int)                                      # TODO
     parser.add_argument('--vis_step', type=int, default=100)
 
-    # save
-    parser.add_argument('--start_epoch', type=int, default=0)
-    parser.add_argument('--log_dir', type=str, default='./logs')
-
     # data
-    parser.add_argument('--data_root', type=str)
-    parser.add_argument('--data_type', type=str)
+    parser.add_argument('--data_root', type=str)                                        # TODO
+    parser.add_argument('--data_type', type=str)                                        # TODO
     parser.add_argument('--num_classes', type=int, default=0)
     parser.add_argument('--num_workers', type=int, default=0)
 
@@ -27,31 +24,25 @@ def get_args_parser():
     parser.add_argument('--warmup_epoch', type=int, default=1)
     parser.add_argument('--weight_decay', type=float, default=5e-4)
     parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--start_epoch', type=int, default=0)
+    parser.add_argument('--log_dir', type=str, default='./logs')
 
     # testing
-    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--test_epoch', type=str, default='best')
     parser.add_argument('--thres', type=float, default=0.05, help='score threshold - 0.05 for test 0.5 for demo')
 
     # demo
-    parser.add_argument('--demo_root', type=str, help='set demo root')
     parser.add_argument('--demo_epoch', type=str, default='best')
+    parser.add_argument('--demo_root', type=str, help='set demo root')                 # TODO
     parser.add_argument('--demo_image_type', type=str, default='jpg')
     parser.set_defaults(demo_vis=False)
     parser.add_argument('--demo_vis_true', dest='demo_vis', action='store_true')
 
-    # for multi-gpu
+    # distributed
+    parser.add_argument('--distributed_true', dest='distributed', action='store_true')
     parser.add_argument('--gpu_ids', nargs="+", default=['0'])   # usage : --gpu_ids 0, 1, 2, 3
     parser.add_argument('--rank', type=int, default=0)
     parser.add_argument('--world_size', type=int, default=1)
 
     return parser
 
-
-if __name__ == '__main__':
-    parser = configargparse.ArgumentParser('faster rcnn training', parents=[get_args_parser()])
-    opts = parser.parse_args()
-
-    opts.world_size = len(opts.gpu_ids)
-    opts.num_workers = len(opts.gpu_ids) * 4
-    print(opts)
