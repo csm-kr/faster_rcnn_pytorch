3afb927609ed0230d4688cf8ccbcf81caf5ceb3c
csm-kr <csm8167@naver.com>
2022-11-13 22:11:49 +0900

    add configs and models for build

---
+++ models/build.py
@@ -0,0 +1,18 @@
+import torch
+from models.model import FRCNN
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+
+def build_model(opts):
+    if opts.distributed:
+        model = FRCNN(num_classes=opts.num_classes)
+        model = model.cuda(int(opts.gpu_ids[opts.rank]))
+        model = DDP(module=model,
+                    device_ids=[int(opts.gpu_ids[opts.rank])],
+                    find_unused_parameters=False)
+    else:
+        # IF DP
+        model = FRCNN(num_classes=opts.num_classes)
+        model = torch.nn.DataParallel(module=model, device_ids=[int(id) for id in opts.gpu_ids])
+    return model
+
